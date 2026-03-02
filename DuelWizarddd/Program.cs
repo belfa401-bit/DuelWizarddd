d6f06b9a1811dd991f98375a06a73f7dcd9b3674
belfa401-bit <flbelfa401@gmail.com>
2026-03-02 08:34:35 +0700

    Menambahkan Aksi

--- DuelWizarddd/Program.cs
+++ DuelWizarddd/Program.cs
@@ -6,13 +6,68 @@ Console.WriteLine("Statistik Awal");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
+String Pilihan;
 
-wizardA.ShowStats();
-wizardB.ShowStats();
+while (true)
 
-Console.WriteLine("Permainan Selesaii!");
+{
+    Console.Clear();
+
+    Console.WriteLine($"1. {wizardA.Name} Menyerang {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} Menyerang {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} Melakukan Heal");
+    Console.WriteLine($"4. {wizardB.Name} Melakukan Heal");
+
+    Console.Write("\nPilihanmu (1/2/3/4): ");
+    Pilihan = Console.ReadLine();
+
+    if (Pilihan == "1")
+    {
+        wizardA.Attack(wizardB);
+    }
+    else if (Pilihan == "2")
+    {
+        wizardB.Attack(wizardA);
+    }
+    else if (Pilihan == "3")
+    {
+        wizardA.Heal();
+    }
+    else if (Pilihan == "4")
+    {
+        wizardB.Heal();
+    }
+    else
+    {
+        Console.WriteLine("Pilihanmu Tidak Valid");
+    }
+
+    if (wizardA.Energy <= 0 || wizardB.Energy <= 0)
+    {
+        Console.WriteLine("Permainan Berakhir!");
+        if (wizardA.Energy > wizardB.Energy)
+        {
+            Console.WriteLine($"{wizardB.Name} Berhasil Dikalahkan!");
+            Console.WriteLine($"{wizardA.Name} Keluar Sebagai Pemenang!");
+        }
+
+        else
+        {
+            Console.WriteLine($"{wizardA.Name} Berhasil Dikalahkan!");
+            Console.WriteLine($"{wizardB.Name} Keluar Sebagai Pemenang!");
+        }
+        break;
+    }
+    Console.ReadLine();
+}
+
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+
+//wizardA.ShowStats();
+//wizardB.ShowStats();
+
+//Console.WriteLine("Permainan Selesaii!");
 
 public class Wizard
 {
